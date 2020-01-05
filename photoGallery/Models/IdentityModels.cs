d8e506d4b1ab7791a0407dbe63004e7e9c9594c4
Rex <im@rex.tw>
2020-01-05 22:26:51 +0800

    增加說明 http://blogs.uuu.com.tw/Articles/post/2018/01/10/ASPNET-Identity-Core%E5%85%A5%E9%96%80-1.aspx

--- photoGallery/Models/IdentityModels.cs
+++ photoGallery/Models/IdentityModels.cs
@@ -49,6 +49,15 @@ namespace photoGallery.Models
             modelBuilder.Entity<IdentityUserRole>().ToTable("AspNetUserRoles");
             modelBuilder.Entity<IdentityUserClaim>().ToTable("AspNetUserClaims");
             modelBuilder.Entity<IdentityUserLogin>().ToTable("AspNetUserLogins");
+
+            // http://blogs.uuu.com.tw/Articles/post/2018/01/10/ASPNET-Identity-Core%E5%85%A5%E9%96%80-1.aspx
+            //IdentityUser ：代表個別使用者資訊。
+            //IdentityRole：代表使用者角色（Role）資訊。
+            //IdentityUserRole：用來定義使用者隸屬於特定的群組。
+            //IdentityUserClaim：代表特定的使用者宣告（Claim）。
+            //IdentityUserLogin：代表使用者登入。
+            //IdentityUserToken：代表一個使用者的驗證（authentication）
+            //IdentityRoleClaim：代表特定的角色宣告（Claim）。
         }
 
     }
