4ef60c6091035258974a8452a89dbc1f586ee322
Rex <im@rex.tw>
2020-01-05 22:23:06 +0800

    預留ASP.NET Identity自訂Table名稱的功能 https://blog.yowko.com/aspnet-identity-2-entityframework-oracle/

--- photoGallery/Models/IdentityModels.cs
+++ photoGallery/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -33,6 +34,21 @@ namespace photoGallery.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        // https://blog.yowko.com/aspnet-identity-2-entityframework-oracle/
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //這必需在第一行
+            base.OnModelCreating(modelBuilder);
+            //schema 名稱，如果建立時沒有刻意指定小寫，預設就是大寫
+            //modelBuilder.HasDefaultSchema("dbo");
+            //以下依實際情境來調整 table name
+            modelBuilder.Entity<ApplicationUser>().ToTable("AspNetUsers");
+            modelBuilder.Entity<IdentityRole>().ToTable("AspNetRoles");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("AspNetUserRoles");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("AspNetUserClaims");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("AspNetUserLogins");
         }
 
     }
